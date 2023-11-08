eb53e64f5fa283355666d9fe8e1f1f3dac321b9b
RodrigoLS <rodrigolopezsoria@gmail.com>
2023-11-08 16:36:32 +0100

    fixed posts

--- db/index.js
+++ db/index.js
@@ -2,7 +2,9 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// ℹ️ Set
+
+// s the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://@greenlet.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000";
