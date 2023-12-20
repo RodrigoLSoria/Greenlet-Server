1a6731cdfd0ca36627f46e672c993f5868c9bfa9
RodrigoLS <rodrigolopezsoria@gmail.com>
2023-12-20 17:25:45 +0100

    port changes 2

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app")
 const http = require('http')
 const server = http.createServer(app)
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 
 require('./config/socket')(server)
 
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 })
